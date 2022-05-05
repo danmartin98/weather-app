cb93d1bd6fea734fb3af57de211d47a3c01ab513
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-05-05 14:34:14 +0100

    initial commit

--- weather/src/Components/App.js
+++ weather/src/Components/App.js
@@ -54,6 +54,8 @@ function App() {
           type="text"
           onChange={handleChange}
         />
+      </div>
+      <div>
         <button className="btn" onClick={handleClick}>
           Search
         </button>
